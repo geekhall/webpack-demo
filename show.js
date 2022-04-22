fe91fb2a6064b17c3d07cc4660a9be140a5debab
geekhall <174821300@qq.com>
2022-04-23 01:05:42 +0800

    refactor: move file

--- show.js
+++
@@ -1,5 +0,0 @@
-function show(content) {
-  // window.document.getElementById('app').innerText = 'Hello, ' + content;
-  window.document.getElementById('app').innerHTML = '<h1>Hello, ' + content + '</h1>';
-}
-module.exports = show;
